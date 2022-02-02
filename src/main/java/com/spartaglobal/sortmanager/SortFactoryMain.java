6985b5494dba2f32b494c2a7de0c84f90b512759
ThistleBlue <c.porteous23@gmail.com>
2022-02-02 16:39:05 +0000

    Changes to the project to introduce logging into the files

--- src/main/java/com/spartaglobal/sortmanager/SortFactoryMain.java
+++ src/main/java/com/spartaglobal/sortmanager/SortFactoryMain.java
@@ -3,14 +3,16 @@ package com.spartaglobal.sortmanager;
 import com.spartaglobal.sortmanager.controller.SortManager;
 import com.spartaglobal.sortmanager.model.NegativeInputException;
 import com.spartaglobal.sortmanager.view.SortView;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class SortFactoryMain {
 
-    
+    public static Logger logger = LogManager.getLogger(SortFactoryMain.class.getName());
 
     public static void main(String[] args) {
+        logger.warn("Execute Order 69");
         SortView sv = new SortView();
         String desiredSortType = sv.getDesiredSort();
         SortManager sm = new SortManager();
